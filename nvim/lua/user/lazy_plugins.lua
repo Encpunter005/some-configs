d161167668f669c0e74d995a437192a6f95d2e8c
Encounter005 <1624221574@qq.com>
2023-05-05 00:18:10 +0800

    disable the animation

--- nvim/lua/user/lazy_plugins.lua
+++ nvim/lua/user/lazy_plugins.lua
@@ -129,44 +129,44 @@ require("lazy").setup({
 	-- UI
 	-- use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
 	--
-	{
-		"echasnovski/mini.animate",
-		version = false,
-		opts = function()
-			-- don't use animate when scrolling with the mouse
-			local mouse_scrolled = false
-			for _, scroll in ipairs({ "Up", "Down" }) do
-				local key = "<ScrollWheel" .. scroll .. ">"
-				vim.keymap.set({ "", "i" }, key, function()
-					mouse_scrolled = true
-					return key
-				end, { expr = true })
-			end
-
-			local animate = require("mini.animate")
-			return {
-				resize = {
-					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
-				},
-				scroll = {
-					timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
-					subscroll = animate.gen_subscroll.equal({
-						predicate = function(total_scroll)
-							if mouse_scrolled then
-								mouse_scrolled = false
-								return false
-							end
-							return total_scroll > 1
-						end,
-					}),
-				},
-			}
-		end,
-		config = function(_, opts)
-			require("mini.animate").setup(opts)
-		end,
-	},
-
+	-- {
+	-- 	"echasnovski/mini.animate",
+	-- 	version = false,
+	-- 	opts = function()
+	-- 		-- don't use animate when scrolling with the mouse
+	-- 		local mouse_scrolled = false
+	-- 		for _, scroll in ipairs({ "Up", "Down" }) do
+	-- 			local key = "<ScrollWheel" .. scroll .. ">"
+	-- 			vim.keymap.set({ "", "i" }, key, function()
+	-- 				mouse_scrolled = true
+	-- 				return key
+	-- 			end, { expr = true })
+	-- 		end
+	--
+	-- 		local animate = require("mini.animate")
+	-- 		return {
+	-- 			resize = {
+	-- 				timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+	-- 			},
+	-- 			scroll = {
+	-- 				timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+	-- 				subscroll = animate.gen_subscroll.equal({
+	-- 					predicate = function(total_scroll)
+	-- 						if mouse_scrolled then
+	-- 							mouse_scrolled = false
+	-- 							return false
+	-- 						end
+	-- 						return total_scroll > 1
+	-- 					end,
+	-- 				}),
+	-- 			},
+	-- 		}
+	-- 	end,
+	-- 	config = function(_, opts)
+	-- 		require("mini.animate").setup(opts)
+	-- 	end,
+	-- },
+	--
 	"glepnir/galaxyline.nvim",
 	--use({"rebelot/heirline.nvim"})
 
