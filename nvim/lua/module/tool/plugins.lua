ecc29cc4bb645828b28c7ac227be787b1295f133
Encounter005 <1624221574@qq.com>
2024-03-22 16:01:27 +0800

    加了一个测试时间的插件

--- nvim/lua/module/tool/plugins.lua
+++ nvim/lua/module/tool/plugins.lua
@@ -367,4 +367,13 @@ return {
             })
         end
     },
+
+    -- measure startuptime
+    {
+        "dstein64/vim-startuptime",
+        cmd = "StartupTime",
+        config = function()
+            vim.g.startuptime_tries = 10
+        end,
+    },
 }
