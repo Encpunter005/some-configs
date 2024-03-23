03eaf8367e9cb781c94236f98bf3a2fc46b6c187
Encounter005 <1624221574@qq.com>
2024-03-23 23:16:46 +0800

    定义了buffer的快捷键

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -62,6 +62,22 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+-- Better Buffer navigation
+
+keymap('n', "<A-1>", "<cmd>BufferGoto 1<CR>", opts)
+keymap('n', "<A-2>", "<cmd>BufferGoto 2<CR>", opts)
+keymap('n', "<A-3>", "<cmd>BufferGoto 3<CR>", opts)
+keymap('n', "<A-4>", "<cmd>BufferGoto 4<CR>", opts)
+keymap('n', "<A-5>", "<cmd>BufferGoto 5<CR>", opts)
+keymap('n', "<A-6>", "<cmd>BufferGoto 6<CR>", opts)
+keymap('n', "<A-7>", "<cmd>BufferGoto 7<CR>", opts)
+keymap('n', "<A-8>", "<cmd>BufferGoto 8<CR>", opts)
+keymap('n', "<A-9>", "<cmd>BufferGoto 9<CR>", opts)
+keymap('n', "<A-0>", "<cmd>BufferGoto 0<CR>", opts)
+keymap('n', "<C-w>", "<cmd>BufferClose<CR>", opts)
+keymap('n', "<A-,>", "<cmd>BufferPrevious<CR>", opts)
+keymap('n', "<A-.>", "<cmd>BufferNext<CR>", opts)
+
 -- Better terminal navigation
 keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
