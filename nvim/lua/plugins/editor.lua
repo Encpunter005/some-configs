d825df7d0fe6fc1c8efab80ea3d61778316dc7f5
Encounter005 <1624221574@qq.com>
2024-03-23 23:20:02 +0800

    修正cmp的图标引用

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -329,7 +329,7 @@ return {
                 },
                 formatting = {
                     format = function(_, item)
-                        local icons = require("config").icons.kinds
+                        local icons = require("config.icons").kinds
                         if icons[item.kind] then
                             item.kind = icons[item.kind] .. item.kind
                         end
